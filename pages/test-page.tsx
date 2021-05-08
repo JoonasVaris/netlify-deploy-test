d5f72eba74e974423d25fff7ca4eb5a438f045c3
JoonasVaris <joonas.varis@valu.fi>
2021-05-08 15:33:56 +0300

    initial

---
+++ pages/test-page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+    return(
+        <div>
+            <h1>My test page</h1>
+            <p>Test text</p>
+        </div>
+    )
+}
+
+export default App;
